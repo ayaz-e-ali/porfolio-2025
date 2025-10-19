8deaed19b4a1db1be8249a6c086d9ce5b9de4811
ayaz-e-ali <hsinsa758@gmail.com>
2025-10-19 17:53:59 +0300

    readded opengraph images

---
+++ app/[projectId]/opengraph-image.jsx
@@ -0,0 +1,42 @@
+import { ImageResponse } from 'next/og'
+import data from '@/data.json'
+// Image metadata
+export const size = {
+    width: 1200,
+    height: 630,
+}
+export const Alt = "Ayaz Ali Project"
+export const contentType = 'image/png'
+
+// Image generation
+export default async function Image({ params }) {
+    const { projectId } = await params;
+    const project = data.projects.find(p => p.id === projectId);
+
+    return new ImageResponse(
+        (
+            // ImageResponse JSX element
+            <div
+                style={{
+                    fontSize: 128,
+                    background: '#222222',
+                    width: '100%',
+                    height: '100%',
+                    display: 'flex',
+                    gap: '2rem',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    color: 'white',
+                }}
+            >
+                <img src="https://ayaz-one.vercel.app/ayaz.png" style={{
+                    height: "300",
+                    width: "300",
+                }} />
+                <p>
+                    {project.title}
+                </p>
+            </div>
+        )
+    )
+}
---
+++ app/opengraph-image.jsx
@@ -0,0 +1,40 @@
+import { ImageResponse } from 'next/og'
+
+// Image metadata
+export const size = {
+    width: 1200,
+    height: 630,
+}
+export const Alt = "Ayaz Ali"
+export const contentType = 'image/png'
+
+// Image generation
+export default async function Image({ params }) {
+
+    return new ImageResponse(
+        (
+            // ImageResponse JSX element
+            <div
+                style={{
+                    fontSize: 128,
+                    background: '#222222',
+                    width: '100%',
+                    height: '100%',
+                    display: 'flex',
+                    gap: '2rem',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    color: 'white',
+                }}
+            >
+                <img src="https://ayaz-one.vercel.app/ayaz.png" style={{
+                    height: "300",
+                    aspectRatio: "1/1",
+                }} />
+                <p>
+                    Ayaz ali
+                </p>
+            </div>
+        )
+    )
+}
